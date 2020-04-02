be5ae1286396f66b9ec4980487fce8aaac2f34b1
undstory <undrul.ozog@gmail.com>
2020-04-02 20:39:03 +0200

    gallery is almost done

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -21,3 +21,58 @@ navItem.forEach(function(e){
 })
 
 //---
+
+let galleryImg = document.querySelectorAll(".gallery__img"); 
+let galleryInfo = document.querySelectorAll(".gallery__info");
+let galleryBtn = document.querySelectorAll(".gallery__btn");
+
+galleryImg.forEach(function(img){
+
+    img.addEventListener("mouseover", function(){
+        
+        img.classList.add('gallery__img--active');
+        
+        let filtr = this.getAttribute('data-filter');
+        console.log(filtr);
+
+        galleryInfo.forEach(function(element){
+
+            element.classList.remove('gallery__info--active');
+
+            if(element.getAttribute('data-filter') === filtr){
+                element.classList.add('gallery__info--active');
+            } 
+            
+        })
+
+        galleryBtn.forEach(function(element){
+
+            element.classList.remove('gallery__btn--active');
+
+            if(element.getAttribute('data-filter') === filtr){
+                element.classList.add('gallery__btn--active');
+            } 
+            
+        })
+
+    })
+
+    img.addEventListener("mouseout", function(){
+        img.classList.remove('gallery__img--active');
+
+        galleryInfo.forEach(function(element){
+
+            element.classList.remove('gallery__info--active');
+            
+        })
+
+        galleryBtn.forEach(function(element){
+
+            element.classList.remove('gallery__btn--active');
+            
+        })
+
+    })
+
+})
+
